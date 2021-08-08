698d7d128fd82cfc14d49c3cde4f7cd3ee001d3c
carlcodling <ccodling@gmail.com>
2021-08-08 18:22:13 +0100

    refactor

--- companion/simple/companion-settings.js
+++ companion/simple/companion-settings.js
@@ -11,7 +11,7 @@ messaging.peerSocket.onopen = () => {
 export function initialize() {
   //settingsStorage.clear();
   console.log("SETTINGS "+settingsStorage.getItem("df"))
-  if(settingsStorage.getItem("df") == null){ // is this the first run
+  if(!settingsStorage.key("df")){ // is this the first run
     setup();
   }
   settingsStorage.addEventListener("change", evt => {
@@ -23,24 +23,20 @@ export function initialize() {
 
 function sendValue(key, val) {
   if (val) {
-    sendSettingData({
+    let data = {
       key: key,
       value: JSON.parse(val)
-    });
-  }
-}
-
-function sendSettingData(data) {
-  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
-    messaging.peerSocket.send(data);
-  } else {
-    console.log("No peerSocket connection");
+    }
+    if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+      messaging.peerSocket.send(data);
+    } else {
+      console.log("No peerSocket connection");
+    }
   }
 }
 
 function setup(){
   let defaults = defaultSettings();
-
   for (const key in defaults) {
     settingsStorage.setItem(key, defaults[key]);
   }
@@ -50,11 +46,7 @@ function restoreSettings(){
   for (let index = 0; index < settingsStorage.length; index++) {
     let key = settingsStorage.key(index);
     if (key) {
-      let data = {
-        key: key,
-        value: JSON.parse(settingsStorage.getItem(key))
-      };
-      sendSettingData(data);
+      sendValue(key, settingsStorage.getItem(key));
     }
   }
 }
